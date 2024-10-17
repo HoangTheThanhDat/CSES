4d20d3792c956c9dc8016e40789fcd73ecb40782
d a t m a . _ c o d e r <121338679+HoangTheThanhDat@users.noreply.github.com>
2024-10-17 16:38:06 +0700

    Update 2431 - Digit Queries.cpp

--- 2431 - Digit Queries.cpp
+++ 2431 - Digit Queries.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 #define ll long long
-#define int ll
 #define fi first 
 #define se second 
 #define el '\n'
@@ -14,52 +13,35 @@ using namespace std;
 template<class A,class B> inline void maximize(A& x, B y) {x = max(x, y);};
 template<class A,class B> inline void minimize(A& x, B y) {x = min(x, y);};
 
-const int N = 2 * 1e5 + 68;
+const int N = 2e5 + 68;
 const int mod = 1e9 + 7;
-const int inf = 2 * 1e9 + 1e8 + 6688;
-const ll oo = 3 * 1e18 + 1e17 + 666888;
-const bool TESTCASE = false;
+const int inf = 2e9 + 1e8 + 6688;
+const ll oo = 3e18 + 1e17 + 666888;
+const bool TESTCASE = true;
 
 //  ------------------- d a t m a . _ c o d e r -------------------  //
 
-int xpow(int x , unsigned int y) {
-    int res = 1;
-
-    while (y > 0) {
-        if (y & 1) res *= x; 
-        y >>= 1; 
-        x *= x;
-    }
-
-    return res;
-}
-
-int t;
-ll n;   
+ll n;
 
 void solve() {
-    cin >> t;
+    cin >> n;
 
-    while (t--) {
-        cin >> n;
+    ll pw = 1 , cs = 1;
 
-        int c = 1;
-
-        for (int p = 9 ;; n -= p , ++c , p = 9 * xpow(10 , c - 1) * c) {
-            if (n - p <= 0) break;
-        }
-
-        --n;
-
-        int x = n / c;
-        int y = n % c;
-
-        int ans = xpow(10 , c - 1) + x;
-
-        string s = to_string(ans);
-
-        cout << s[y] << el;
+    while (n - 9 * cs * pw > 0) {
+        n -= 9 * cs * pw;
+        ++cs;
+        pw *= 10;
     }
+
+    ll a = pw + (n - 1) / cs;
+
+    n %= cs;
+
+    string so = to_string(a);
+
+    if (n == 0) cout << so.back(); else cout << so[n - 1];
+    cout << el;
 }
 
 datmacoder {
