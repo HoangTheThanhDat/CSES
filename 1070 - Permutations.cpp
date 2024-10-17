2b9466b6c8736ba69ffbf0ed76ee5eab899d7afb
d a t m a . _ c o d e r <121338679+HoangTheThanhDat@users.noreply.github.com>
2024-10-17 15:00:35 +0700

    Update 1070 - Permutations.cpp

--- 1070 - Permutations.cpp
+++ 1070 - Permutations.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
+ 
 #define ll long long
 #define fi first 
 #define se second 
@@ -9,51 +9,63 @@ using namespace std;
 #define reset(__a , __v) memset(__a,__v,sizeof(__a))
 #define datmacoder int32_t main()
 #define openfile(__dat) if (fopen(__dat".inp","r")){freopen(__dat".inp","r",stdin);freopen(__dat".out","w",stdout);}
-
+ 
 template<class A,class B> inline void maximize(A& x, B y) {x = max(x, y);};
 template<class A,class B> inline void minimize(A& x, B y) {x = min(x, y);};
-
+ 
 const int N = 2 * 1e5 + 68;
 const int mod = 1e9 + 7;
 const int inf = 2 * 1e9 + 1e8 + 6688;
 const ll oo = 3 * 1e18 + 1e17 + 666888;
-const bool TESTCASE = true;
-
+const bool TESTCASE = false;
+ 
 //  ------------------- d a t m a . _ c o d e r -------------------  //
-
-
-vector<int> a[6];
-int x , y;
-
+ 
+int n , a[N];
+ 
 void solve() {
-    cin >> x >> y;
-
-    cout << a[x][y] << el;
+    cin >> n;
+ 
+    if (n == 1) cout << 1 << el;
+    else {
+        if (n == 2 || n == 3) cout << "NO SOLUTION" << el;
+        else {
+            int i = 2;
+ 
+            while (i <= n) {
+                cout << i << " ";
+ 
+                i += 2;
+            }
+ 
+            i = 1;
+ 
+            while (i <= n) {
+                cout << i << " ";
+ 
+                i += 2;
+            }
+        } 
+    }
 }
-
+ 
 datmacoder {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-
+ 
     openfile("txt");
-
+ 
     int testcase;
     if (!TESTCASE) testcase = 1;
     else cin >> testcase;
-    
-    a[1] = {0 , 1 , 2 , 9 , 10 , 25};
-    a[2] = {0 , 4 , 3 , 8 , 11 , 24};
-    a[3] = {0 , 5 , 6 , 7 , 12 , 23};
-    a[4] = {0 , 16 , 15 , 14 , 13 , 22};
-    a[5] = {9 , 17 , 18 , 19 , 20 , 21};
-
+ 
     while (testcase--) {
         solve();
     }
-
+ 
     return 0;
 }
-
+ 
 /* 
   d a t m a . _ c o d e r
   H O A N G  T H E  T H A N H  D A T
