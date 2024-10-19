350dacb34b68f5392e728af5bccea2f369005257
d a t m a . _ c o d e r <121338679+HoangTheThanhDat@users.noreply.github.com>
2024-10-19 14:27:35 +0700

    Update 2162 - Josephus Problem I.cpp

--- 2162 - Josephus Problem I.cpp
+++ 2162 - Josephus Problem I.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
+
 #define ll long long
 #define fi first 
 #define se second 
@@ -9,55 +9,59 @@ using namespace std;
 #define reset(__a , __v) memset(__a,__v,sizeof(__a))
 #define datmacoder int32_t main()
 #define openfile(__dat) if (fopen(__dat".inp","r")){freopen(__dat".inp","r",stdin);freopen(__dat".out","w",stdout);}
- 
+
 template<class A,class B> inline void maximize(A& x, B y) {x = max(x, y);};
 template<class A,class B> inline void minimize(A& x, B y) {x = min(x, y);};
- 
+
 const int N = 2e5 + 68;
 const int mod = 1e9 + 7;
 const int inf = 2e9 + 1e8 + 6688;
 const ll oo = 3e18 + 1e17 + 666888;
 const bool TESTCASE = false;
- 
+
 //  ------------------- d a t m a . _ c o d e r -------------------  //
- 
+
 int n;
- 
+queue<int> q;
+
 void solve() {
     cin >> n;
- 
-    int a = 1 , b = 0;
- 
-    while (n > 0) {
-        for (int i = 2 ; i <= n ; i += 2) {
-            cout << a * i + b << " ";
+
+    for (int i = 1 ; i <= n ; i++) q.push(i);
+
+    bool check = false;
+
+    while (q.size() > 0) {
+
+        int value = q.front();
+        q.pop();
+
+        if (!check) q.push(value);
+        else {
+            cout << value << " ";
         }
- 
-        if (n & 1) cout << a + b << " " , b += a;
-        else b -= a;
- 
-        a <<= 1;
-        n >>= 1;
+
+        check = !check;
     }
 }
- 
+
 datmacoder {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
- 
+
     openfile("txt");
- 
+
     int testcase;
     if (!TESTCASE) testcase = 1;
     else cin >> testcase;
- 
+
     while (testcase--) {
         solve();
     }
- 
+
     return 0;
 }
- 
+
 /* 
   d a t m a . _ c o d e r
   H O A N G  T H E  T H A N H  D A T
