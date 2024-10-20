3918367ea68412c38c09490c19c9f25fa6fabfb4
d a t m a . _ c o d e r <121338679+HoangTheThanhDat@users.noreply.github.com>
2024-10-20 23:02:27 +0700

    Update 2431 - Digit Queries.cpp

--- 2431 - Digit Queries.cpp
+++ 2431 - Digit Queries.cpp
@@ -24,44 +24,44 @@ const bool TESTCASE = true;
 ll n;
 
 void solve() {
-    cin >> n;
+        cin >> n;
 
-    ll pw = 1 , cs = 1;
+        ll pw = 1 , cs = 1;
 
-    while (n - 9 * cs * pw > 0) {
-        n -= 9 * cs * pw;
-        ++cs;
-        pw *= 10;
-    }
+        while (n - 9 * cs * pw > 0) {
+                n -= 9 * cs * pw;
+                ++cs;
+                pw *= 10;
+        }
 
-    ll a = pw + (n - 1) / cs;
+        ll a = pw + (n - 1) / cs;
 
-    n %= cs;
+        n %= cs;
 
-    string so = to_string(a);
+        string so = to_string(a);
 
-    if (n == 0) cout << so.back(); else cout << so[n - 1];
-    cout << el;
+        if (n == 0) cout << so.back(); else cout << so[n - 1];
+        cout << el;
 }
 
 datmacoder {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL); cout.tie(NULL);
 
-    openfile("txt");
+        openfile("txt");
 
-    int testcase;
-    if (!TESTCASE) testcase = 1;
-    else cin >> testcase;
+        int testcase;
+        if (!TESTCASE) testcase = 1;
+        else cin >> testcase;
 
-    while (testcase--) {
-        solve();
-    }
+        while (testcase--) {
+                solve();
+        }
 
-    return 0;
+        return 0;
 }
 
 /* 
-  d a t m a . _ c o d e r
-  H O A N G  T H E  T H A N H  D A T
+        d a t m a . _ c o d e r
+        H O A N G  T H E  T H A N H  D A T
 */
