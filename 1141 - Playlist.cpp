309ceb162f533bba0281c6df19f927ca00d4c167
d a t m a . _ c o d e r <121338679+HoangTheThanhDat@users.noreply.github.com>
2024-10-20 22:28:46 +0700

    Update 1141 - Playlist.cpp

--- 1141 - Playlist.cpp
+++ 1141 - Playlist.cpp
@@ -25,50 +25,48 @@ int n , a[N] , cur , ans;
 set<pair<int , int>> s;
 
 void solve() {
-    cin >> n;
+        cin >> n;
 
-    cur = 1; ans = 0;
+        cur = 1; ans = 0;
 
-    for (int i = 1 ; i <= n ; i++) {
-        cin >> a[i];
+        for (int i = 1 ; i <= n ; i++) {
+                cin >> a[i];
 
-        set<pair<int , int>>::iterator x = s.lower_bound({a[i] , -1});
+                set<pair<int , int>>::iterator x = s.lower_bound({a[i] , -1});
 
-        if (x != s.end() && x -> fi == a[i]) {
+                if (x != s.end() && x -> fi == a[i]) {
+                        for (int j = cur ; j <= x -> se ; j++) 
+                                s.erase({a[j] , j});
 
-            for (int j = cur ; j <= x -> se ; j++) {
-                s.erase({a[j] , j});
-            }
+                        cur = x -> se + 1;
+                }
 
-            cur = x -> se + 1;
+                s.insert({a[i] , i});
+
+                maximize(ans , (int)s.size());
         }
 
-        s.insert({a[i] , i});
-
-        maximize(ans , (int)s.size());
-    }
-
-    cout << ans;
+        cout << ans;
 }
 
 datmacoder {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL); cout.tie(NULL);
 
-    openfile("txt");
+        openfile("txt");
 
-    int testcase;
-    if (!TESTCASE) testcase = 1;
-    else cin >> testcase;
+        int testcase;
+        if (!TESTCASE) testcase = 1;
+        else cin >> testcase;
 
-    while (testcase--) {
-        solve();
-    }
+        while (testcase--) {
+            solve();
+        }
 
-    return 0;
+        return 0;
 }
 
 /* 
-  d a t m a . _ c o d e r
-  H O A N G  T H E  T H A N H  D A T
+        d a t m a . _ c o d e r
+        H O A N G  T H E  T H A N H  D A T
 */
