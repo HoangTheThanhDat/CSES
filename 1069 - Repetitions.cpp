751dddd837d6b86b16894430dd0cc1c1a45e50ec
d a t m a . _ c o d e r <121338679+HoangTheThanhDat@users.noreply.github.com>
2024-10-20 21:44:24 +0700

    Update 1069 - Repetitions.cpp

--- 1069 - Repetitions.cpp
+++ 1069 - Repetitions.cpp
@@ -35,8 +35,8 @@ void solve() {
         for (int i = 1 ; i < s.size() ; i++) 
                 if (s[i] == s[i - 1]) 
                         ++d;
-                    else 
-                            maximize(ans , d) ,
+                else 
+                        maximize(ans , d) ,
                         d = 1;
         
 
