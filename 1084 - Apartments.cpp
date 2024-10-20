1dd2ce1e9348e0308c04dc78381dfdf78e64c039
d a t m a . _ c o d e r <121338679+HoangTheThanhDat@users.noreply.github.com>
2024-10-20 21:49:59 +0700

    Update 1084 - Apartments.cpp

--- 1084 - Apartments.cpp
+++ 1084 - Apartments.cpp
@@ -25,49 +25,47 @@ int n , m , k;
 int a[N] , b[N];
 
 void solve() {
-    cin >> n >> m >> k;
+        cin >> n >> m >> k;
 
-    for (int i = 1 ; i <= n ; i++) cin >> a[i];
+        for (int i = 1 ; i <= n ; i++) cin >> a[i];
 
-    for (int i = 1 ; i <= m ; i++) cin >> b[i];
+        for (int i = 1 ; i <= m ; i++) cin >> b[i];
 
-    sort(a + 1 , a + n + 1);
-    sort(b + 1 , b + m + 1);
+        sort(a + 1 , a + n + 1);
+        sort(b + 1 , b + m + 1);
 
-    int i = 1 , j = 1 , ans = 0;
+        int i = 1 , j = 1 , ans = 0;
 
-    while (i <= n && j <= m) {
-        if (a[i] > b[j] + k) ++j;
-        else {
-            if (a[i] < b[j] - k) ++i;
-            else {
-                ++i; ++j;
-                ++ans;
-            }
-        }
-    }
+        while (i <= n && j <= m) 
+                if (a[i] > b[j] + k) 
+                        ++j;
+                else 
+                        if (a[i] < b[j] - k) 
+                                ++i;
+                        else 
+                                ++i , ++j , ++ans;
 
-    cout << ans;
+        cout << ans;
 }
 
 datmacoder {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL); cout.tie(NULL);
 
-    openfile("txt");
+        openfile("txt");
 
-    int testcase;
-    if (!TESTCASE) testcase = 1;
-    else cin >> testcase;
+        int testcase;
+        if (!TESTCASE) testcase = 1;
+        else cin >> testcase;
 
-    while (testcase--) {
-        solve();
-    }
+        while (testcase--) {
+                solve();
+        }
 
-    return 0;
+        return 0;
 }
 
 /* 
-  d a t m a . _ c o d e r
-  H O A N G  T H E  T H A N H  D A T
+        d a t m a . _ c o d e r
+        H O A N G  T H E  T H A N H  D A T
 */
