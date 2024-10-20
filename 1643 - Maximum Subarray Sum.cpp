e3e544e4ac496a85a03cd382ebc8ff483ddd4da6
d a t m a . _ c o d e r <121338679+HoangTheThanhDat@users.noreply.github.com>
2024-10-20 22:49:16 +0700

    Update 1643 - Maximum Subarray Sum.cpp

--- 1643 - Maximum Subarray Sum.cpp
+++ 1643 - Maximum Subarray Sum.cpp
@@ -25,39 +25,39 @@ int n , so;
 ll dp[N] , ans = -oo;
 
 void solve() {
-    cin >> n;
+        cin >> n;
 
-    for (int i = 1 ; i <= n ; i++) {
-        cin >> so;
+        for (int i = 1 ; i <= n ; i++) {
+                cin >> so;
 
-        dp[i] = so;
+                dp[i] = so;
 
-        maximize(dp[i] , dp[i - 1] + so);
+                maximize(dp[i] , dp[i - 1] + so);
 
-        maximize(ans , dp[i]);
-    }
+                maximize(ans , dp[i]);
+        }
 
-    cout << ans;
+        cout << ans;
 }
 
 datmacoder {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL); cout.tie(NULL);
 
-    openfile("txt");
+        openfile("txt");
 
-    int testcase;
-    if (!TESTCASE) testcase = 1;
-    else cin >> testcase;
+        int testcase;
+        if (!TESTCASE) testcase = 1;
+        else cin >> testcase;
 
-    while (testcase--) {
-        solve();
-    }
+        while (testcase--) {
+                solve();
+        }
 
-    return 0;
+        return 0;
 }
 
 /* 
-  d a t m a . _ c o d e r
-  H O A N G  T H E  T H A N H  D A T
+        d a t m a . _ c o d e r
+        H O A N G  T H E  T H A N H  D A T
 */
