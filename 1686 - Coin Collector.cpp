edbbce583f725feec7d1c41b1fe5bea8bbf9e48c
d a t m a . _ c o d e r <121338679+HoangTheThanhDat@users.noreply.github.com>
2024-12-07 14:50:44 +0700

    Add files via upload

---
+++ 1686 - Coin Collector.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define      ll      long long
+#define      fi      first 
+#define      se      second 
+#define      el      '\n'
+
+#define reset(__a , __v) memset(__a,__v,sizeof(__a))
+#define datmacoder int32_t main()
+#define opf(__dat) if (fopen(__dat".inp","r")){freopen(__dat".inp","r",stdin);freopen(__dat".out","w",stdout);}
+#define openfile(__dat) opf(__dat);int _t;if(!TESTCASE)_t=1;else cin>>_t;while(_t--){solve();} 
+
+template<class A,class B> inline void maximize(A& x, B y) {x = max(x, y);};
+template<class A,class B> inline void minimize(A& x, B y) {x = min(x, y);};
+
+const int maxn = 2e5 + 68;
+const int mod = 1e9 + 7;
+const int inf = 0x3f3f3f3f;
+const ll infll = 0x3f3f3f3f3f3f3f3f;
+const bool TESTCASE = false;
+
+//  ------------------- d a t m a . _ c o d e r -------------------  //
+
+int n , m , k[maxn] , u , v;
+vector<int> e[maxn];
+vector<int> escc[maxn];
+int deg[maxn];
+ll sum[maxn] , dp[maxn];
+
+int low[maxn] , num[maxn] , timedfs = 0 , scc = 0 , id[maxn];
+stack<int> s;
+queue<int> q;
+
+void tarjan(int u) {
+        low[u] = num[u] = ++timedfs;
+
+        s.push(u);
+
+        for (int v : e[u]) 
+                if (!id[v]) 
+                        if (!num[v]) {
+                                tarjan(v);
+                                low[u] = min(low[u] , low[v]);
+                        }
+                        else    
+                                low[u] = min(low[u] , num[v]);
+        
+        if (num[u] == low[u]) {
+                ++scc;
+
+                int v;
+
+                do {
+                        v = s.top();
+                        s.pop();
+
+                        id[v] = scc;
+                } while (v != u);
+        }
+}
+
+void solve() {
+        cin >> n >> m;
+
+        for (int i = 1 ; i <= n ; i++) cin >> k[i];
+
+        for (int i = 1 ; i <= m ; i++) {
+                cin >> u >> v;
+
+                e[u].push_back(v);
+        }
+
+        for (int i = 1 ; i <= n ; i++) 
+                if (!id[i]) 
+                        tarjan(i);
+
+        for (int i = 1 ; i <= n ; i++) 
+                sum[id[i]] += k[i];
+
+        for (int i = 1 ; i <= n ; i++) 
+                for (int j : e[i]) 
+                        if (id[i] != id[j]) 
+                                escc[id[i]].push_back(id[j]) , 
+                                ++deg[id[j]];
+
+        reset(dp , 0);
+
+        for (int i = 1 ; i <= scc ; i++) 
+                if (deg[i] == 0) q.push(i) , dp[i] = sum[i];
+
+        while (q.size() > 0) {
+                int u = q.front();
+                q.pop();
+
+                for (int v : escc[u]) {
+                        dp[v] = max(dp[v] , dp[u] + sum[v]);
+                        
+                        --deg[v];
+
+                        if (deg[v] == 0) q.push(v);
+                }
+        }
+
+        cout << *max_element(dp + 1 , dp + scc + 1) << el;
+}
+
+datmacoder {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL); cout.tie(NULL);
+
+        openfile("nofile");
+
+        return 0;
+}
+
+/* 
+        d a t m a . _ c o d e r
+        H O A N G   T H E   T H A N H   D A T
+*/
