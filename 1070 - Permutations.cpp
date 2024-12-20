c0d9be843acb956f9f2ac9ff2d7dda3ac6d968ab
d a t m a . _ c o d e r <121338679+HoangTheThanhDat@users.noreply.github.com>
2024-12-20 17:50:23 +0700

    Update 1070 - Permutations.cpp

--- 1070 - Permutations.cpp
+++ 1070 - Permutations.cpp
@@ -1,72 +1,23 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
-#define ll long long
-#define fi first 
-#define se second 
-#define el '\n'
-#define reset(__a , __v) memset(__a,__v,sizeof(__a))
-#define datmacoder int32_t main()
-#define openfile(__dat) if (fopen(__dat".inp","r")){freopen(__dat".inp","r",stdin);freopen(__dat".out","w",stdout);}
- 
-template<class A,class B> inline void maximize(A& x, B y) {x = max(x, y);};
-template<class A,class B> inline void minimize(A& x, B y) {x = min(x, y);};
- 
-const int N = 2 * 1e5 + 68;
-const int mod = 1e9 + 7;
-const int inf = 2 * 1e9 + 1e8 + 6688;
-const ll oo = 3 * 1e18 + 1e17 + 666888;
-const bool TESTCASE = false;
- 
-//  ------------------- d a t m a . _ c o d e r -------------------  //
- 
-int n , a[N];
- 
-void solve() {
-        cin >> n;
- 
-        if (n == 1) 
-                cout << 1 << el;
-        else
-                if (n == 2 || n == 3) cout << "NO SOLUTION" << el;
-                else {
-                        int i = 2;
- 
-                        while (i <= n) {
-                                cout << i << " ";
- 
-                                i += 2;
-                        }
- 
-                        i = 1;
- 
-                        while (i <= n) {
-                                cout << i << " ";
- 
-                                i += 2;
-                        }
-                } 
-}
- 
-datmacoder {
+
+int n;
+
+int32_t main() {
         ios_base::sync_with_stdio(false);
-        cin.tie(NULL); cout.tie(NULL);
- 
-        openfile("txt");
- 
-        int testcase;
-        if (!TESTCASE) testcase = 1;
-        else cin >> testcase;
- 
-        while (testcase--) {
-                solve();
+        cin.tie(0); cout.tie(0);
+
+        cin >> n;
+
+        if (n == 1)     
+                cout << 1;
+        else 
+                if (n == 2 || n == 3) cout << "NO SOLUTION";
+        else {
+                for (int i = 2 ; i <= n ; i += 2) cout << i << ' ';
+                for (int i = 1 ; i <= n ; i += 2) cout << i << ' ';
         }
- 
+
         return 0;
 }
- 
-/* 
-       d a t m a . _ c o d e r
-       H O A N G   T H E   T H A N H   D A T
-*/
