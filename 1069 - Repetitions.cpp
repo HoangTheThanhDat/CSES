86921a23e94166baa3bc4ca15246c32c0d2bda64
d a t m a . _ c o d e r <121338679+HoangTheThanhDat@users.noreply.github.com>
2024-12-20 15:44:38 +0700

    Update 1069 - Repetitions.cpp

--- 1069 - Repetitions.cpp
+++ 1069 - Repetitions.cpp
@@ -2,65 +2,26 @@
 
 using namespace std;
 
-#define ll long long
-#define fi first 
-#define se second 
-#define el '\n'
-#define reset(__a , __v) memset(__a,__v,sizeof(__a))
-#define datmacoder int32_t main()
-#define openfile(__dat) if (fopen(__dat".inp","r")){freopen(__dat".inp","r",stdin);freopen(__dat".out","w",stdout);}
+string st;
 
-template<class A,class B> inline void maximize(A& x, B y) {x = max(x, y);};
-template<class A,class B> inline void minimize(A& x, B y) {x = min(x, y);};
+int32_t main() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0); cout.tie(0);
 
-const int N = 2 * 1e5 + 68;
-const int mod = 1e9 + 7;
-const int inf = 2 * 1e9 + 1e8 + 6688;
-const ll oo = 3 * 1e18 + 1e17 + 666888;
-const bool TESTCASE = false;
+        cin >> st;
 
-//  ------------------- d a t m a . _ c o d e r -------------------  //
+        int ans = 1 , cnt = 1;
 
-string s;
-
-void solve() {
-        cin >> s;
-
-        s = s + 'a';
-
-        int d = 1;
-
-        int ans = 0;
-
-        for (int i = 1 ; i < s.size() ; i++) 
-                if (s[i] == s[i - 1]) 
-                        ++d;
-                else 
-                        maximize(ans , d) ,
-                        d = 1;
+        for (int i = 1 ; i < st.size() ; i++) 
+                if (st[i] == st[i - 1]) ++cnt;
+                else {
+                        ans = max(ans , cnt);
+                        cnt = 1;
+                }
         
+        ans = max(ans , cnt);
 
         cout << ans;
-}
-
-datmacoder {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL); cout.tie(NULL);
-
-        openfile("txt");
-
-        int testcase;
-        if (!TESTCASE) testcase = 1;
-        else cin >> testcase;
-
-        while (testcase--) {
-                solve();
-        }
 
         return 0;
 }
-
-/* 
-        d a t m a . _ c o d e r
-        H O A N G   T H E   T H A N H   D A T
-*/
