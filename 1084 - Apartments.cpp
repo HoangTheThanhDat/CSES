700717be3e3660af65066c965ddd0141f9f7330e
d a t m a . _ c o d e r <121338679+HoangTheThanhDat@users.noreply.github.com>
2024-12-21 23:20:18 +0700

    Update 1084 - Apartments.cpp

--- 1084 - Apartments.cpp
+++ 1084 - Apartments.cpp
@@ -2,33 +2,18 @@
 
 using namespace std;
 
-#define ll long long
-#define fi first 
-#define se second 
-#define el '\n'
-#define reset(__a , __v) memset(__a,__v,sizeof(__a))
-#define datmacoder int32_t main()
-#define openfile(__dat) if (fopen(__dat".inp","r")){freopen(__dat".inp","r",stdin);freopen(__dat".out","w",stdout);}
-
-template<class A,class B> inline void maximize(A& x, B y) {x = max(x, y);};
-template<class A,class B> inline void minimize(A& x, B y) {x = min(x, y);};
-
-const int N = 2 * 1e5 + 68;
-const int mod = 1e9 + 7;
-const int inf = 2 * 1e9 + 1e8 + 6688;
-const ll oo = 3 * 1e18 + 1e17 + 666888;
-const bool TESTCASE = false;
-
-//  ------------------- d a t m a . _ c o d e r -------------------  //
+const int maxn = 2e5 + 68;
 
 int n , m , k;
-int a[N] , b[N];
+int a[maxn] , b[maxn];
 
-void solve() {
+int32_t main() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0); cout.tie(0);
+
         cin >> n >> m >> k;
 
         for (int i = 1 ; i <= n ; i++) cin >> a[i];
-
         for (int i = 1 ; i <= m ; i++) cin >> b[i];
 
         sort(a + 1 , a + n + 1);
@@ -36,7 +21,7 @@ void solve() {
 
         int i = 1 , j = 1 , ans = 0;
 
-        while (i <= n && j <= m) 
+        while (i <= n && j <= m) {
                 if (a[i] > b[j] + k) 
                         ++j;
                 else 
@@ -44,28 +29,9 @@ void solve() {
                                 ++i;
                         else 
                                 ++i , ++j , ++ans;
+        }
 
         cout << ans;
-}
-
-datmacoder {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL); cout.tie(NULL);
-
-        openfile("txt");
-
-        int testcase;
-        if (!TESTCASE) testcase = 1;
-        else cin >> testcase;
-
-        while (testcase--) {
-                solve();
-        }
 
         return 0;
 }
-
-/* 
-        d a t m a . _ c o d e r
-        H O A N G   T H E   T H A N H   D A T
-*/
