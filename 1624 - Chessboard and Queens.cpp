15bf296ccaa59e2eaaeec927b8c160d80e435fa0
d a t m a . _ c o d e r <121338679+HoangTheThanhDat@users.noreply.github.com>
2024-12-21 22:32:49 +0700

    Update 1624 - Chessboard and Queens.cpp

--- 1624 - Chessboard and Queens.cpp
+++ 1624 - Chessboard and Queens.cpp
@@ -2,29 +2,12 @@
 
 using namespace std;
 
-#define ll long long
-#define fi first 
-#define se second 
-#define el '\n'
-#define reset(__a , __v) memset(__a,__v,sizeof(__a))
-#define datmacoder int32_t main()
-#define openfile(__dat) if (fopen(__dat".inp","r")){freopen(__dat".inp","r",stdin);freopen(__dat".out","w",stdout);}
+const int maxn = 8 + 2;
 
-template<class A,class B> inline void maximize(A& x, B y) {x = max(x, y);};
-template<class A,class B> inline void minimize(A& x, B y) {x = min(x, y);};
+string chess[maxn];
+bool row[maxn] , dmain[maxn + maxn] , dsecon[maxn + maxn];
 
-const int N = 2e5 + 68;
-const int mod = 1e9 + 7;
-const int inf = 2e9 + 1e8 + 6688;
-const ll oo = 3e18 + 1e17 + 666888;
-const bool TESTCASE = false;
-
-//  ------------------- d a t m a . _ c o d e r -------------------  //
-
-string chess[10];
-bool row[10 + 10] , dmain[10 + 10] , dsecon[10 + 10];
-
-ll ans = 0;
+long long ans = 0;
 
 void rec(int col) {
         if (col == 9) {
@@ -33,51 +16,34 @@ void rec(int col) {
         }
 
         for (int i = 1 ; i <= 8 ; i++) 
-                if (!row[i] && !dmain[i - col + 8] && !dsecon[i + col] && chess[col][i] == '.') {   
+                if (!row[i] && !dmain[i - col + 8] && !dsecon[i + col] && chess[col][i] == '.') {
 
                         row[i] = dmain[i - col + 8] = dsecon[i + col] = true;
-                        
+
                         rec(col + 1);
 
                         row[i] = dmain[i - col + 8] = dsecon[i + col] = false;
                 }
 }
 
-void solve() {
+int32_t main() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0); cout.tie(0);
+
         for (int i = 1 ; i <= 8 ; i++) {
                 cin >> chess[i];
                 chess[i] = " " + chess[i];
         }
 
-        reset(row , false);
-        reset(dmain , false);
-        reset(dsecon , false);
+        memset(row , false , sizeof row);
+        memset(dmain , false , sizeof dmain);
+        memset(dsecon , false , sizeof dsecon);
 
         ans = 0;
 
         rec(1);
 
         cout << ans;
-}
-
-datmacoder {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL); cout.tie(NULL);
-
-        openfile("txt");
-
-        int testcase;
-        if (!TESTCASE) testcase = 1;
-        else cin >> testcase;
-
-        while (testcase--) {
-                solve();
-        }
 
         return 0;
 }
-
-/* 
-        d a t m a . _ c o d e r
-        H O A N G   T H E   T H A N H   D A T
-*/
