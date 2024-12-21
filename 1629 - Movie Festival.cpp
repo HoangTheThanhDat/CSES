c9ecdc35db5573f418aa1a016019e2adb411c0cb
d a t m a . _ c o d e r <121338679+HoangTheThanhDat@users.noreply.github.com>
2024-12-21 23:32:39 +0700

    Update 1629 - Movie Festival.cpp

--- 1629 - Movie Festival.cpp
+++ 1629 - Movie Festival.cpp
@@ -2,65 +2,29 @@
 
 using namespace std;
 
-#define ll long long
-#define fi first 
-#define se second 
-#define el '\n'
-#define reset(__a , __v) memset(__a,__v,sizeof(__a))
-#define datmacoder int32_t main()
-#define openfile(__dat) if (fopen(__dat".inp","r")){freopen(__dat".inp","r",stdin);freopen(__dat".out","w",stdout);}
-
-template<class A,class B> inline void maximize(A& x, B y) {x = max(x, y);};
-template<class A,class B> inline void minimize(A& x, B y) {x = min(x, y);};
-
-const int N = 2e5 + 68;
-const int mod = 1e9 + 7;
-const int inf = 2e9 + 1e8 + 6688;
-const ll oo = 3e18 + 1e17 + 666888;
-const bool TESTCASE = false;
-
-//  ------------------- d a t m a . _ c o d e r -------------------  //
+const int maxn = 2e5 + 68;
 
 int n;
-pair<int , int> a[N];
+pair<int , int> a[maxn];
 
-bool cmp(pair<int , int> a , pair<int , int> b) {
-        return a.se < b.se;
-}
+int32_t main() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0); cout.tie(0);
 
-void solve() {
         cin >> n;
 
-        for (int i = 1 ; i <= n ; i++) cin >> a[i].fi >> a[i].se;
+        for (int i = 1 ; i <= n ; i++) cin >> a[i].first >> a[i].second;
 
-        sort(a + 1 , a + n + 1 , cmp);
+        sort(a + 1 , a + n + 1 , [](pair<int , int> &x , pair<int , int> &y) {
+                return x.second < y.second;
+        });
 
-        int ans = 1 , cur = a[1].se;
+        int ans = 1 , cur = a[1].second;
 
         for (int i = 2 ; i <= n ; i++) 
-                if (a[i].fi >= cur) ++ans , cur = a[i].se;
-
+                if (a[i].first >= cur) ++ans , cur = a[i].second;
+        
         cout << ans;
-}
-
-datmacoder {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL); cout.tie(NULL);
-
-        openfile("txt");
-
-        int testcase;
-        if (!TESTCASE) testcase = 1;
-        else cin >> testcase;
-
-        while (testcase--) {
-                solve();
-        }
 
         return 0;
 }
-
-/* 
-        d a t m a . _ c o d e r
-        H O A N G   T H E   T H A N H   D A T
-*/
