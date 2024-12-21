d50ed7243bb006566a9a626590114da6022b3c9e
d a t m a . _ c o d e r <121338679+HoangTheThanhDat@users.noreply.github.com>
2024-12-21 22:35:34 +0700

    Update 2431 - Digit Queries.cpp

--- 2431 - Digit Queries.cpp
+++ 2431 - Digit Queries.cpp
@@ -2,66 +2,39 @@
 
 using namespace std;
 
-#define ll long long
-#define fi first 
-#define se second 
-#define el '\n'
-#define reset(__a , __v) memset(__a,__v,sizeof(__a))
-#define datmacoder int32_t main()
-#define openfile(__dat) if (fopen(__dat".inp","r")){freopen(__dat".inp","r",stdin);freopen(__dat".out","w",stdout);}
+int t;
+long long n;
 
-template<class A,class B> inline void maximize(A& x, B y) {x = max(x, y);};
-template<class A,class B> inline void minimize(A& x, B y) {x = min(x, y);};
-
-const int N = 2e5 + 68;
-const int mod = 1e9 + 7;
-const int inf = 2e9 + 1e8 + 6688;
-const ll oo = 3e18 + 1e17 + 666888;
-const bool TESTCASE = true;
-
-//  ------------------- d a t m a . _ c o d e r -------------------  //
-
-ll n;
-
-void solve() {
-        cin >> n;
-
-        ll pw = 1 , cs = 1;
-
-        while (n - 9 * cs * pw > 0) {
-                n -= 9 * cs * pw;
-                ++cs;
-                pw *= 10;
-        }
-
-        ll a = pw + (n - 1) / cs;
-
-        n %= cs;
-
-        string so = to_string(a);
-
-        if (n == 0) cout << so.back(); else cout << so[n - 1];
-        cout << el;
-}
-
-datmacoder {
+int32_t main() {
         ios_base::sync_with_stdio(false);
-        cin.tie(NULL); cout.tie(NULL);
+        cin.tie(0); cout.tie(0);
 
-        openfile("txt");
+        cin >> t;
 
-        int testcase;
-        if (!TESTCASE) testcase = 1;
-        else cin >> testcase;
+        while (t--) {
+                cin >> n;
 
-        while (testcase--) {
-                solve();
+                long long pw = 1 , cs = 1;
+
+                while (n - 9 * cs * pw > 0) {
+                        n -= 9 * cs * pw;
+                        ++cs;
+                        pw *= 10;
+                }
+
+                long long a = pw + (n - 1) / cs;
+
+                n %= cs;
+
+                string so = to_string(a);
+
+                if (n == 0) 
+                        cout << so.back();
+                else 
+                        cout << so[n - 1];
+                
+                cout << '\n';
         }
 
         return 0;
 }
-
-/* 
-        d a t m a . _ c o d e r
-        H O A N G   T H E   T H A N H   D A T
-*/
