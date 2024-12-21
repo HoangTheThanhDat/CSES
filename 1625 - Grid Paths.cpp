2ed71a9d91aee20a933467ea7f6888a8a5b5868a
d a t m a . _ c o d e r <121338679+HoangTheThanhDat@users.noreply.github.com>
2024-12-21 23:01:26 +0700

    Update 1625 - Grid Paths.cpp

--- 1625 - Grid Paths.cpp
+++ 1625 - Grid Paths.cpp
@@ -2,55 +2,40 @@
 
 using namespace std;
 
-#define ll long long
-#define fi first 
-#define se second 
-#define el '\n'
-#define reset(__a , __v) memset(__a,__v,sizeof(__a))
-#define datmacoder int32_t main()
-#define openfile(__dat) if (fopen(__dat".inp","r")){freopen(__dat".inp","r",stdin);freopen(__dat".out","w",stdout);}
-
-template<class A,class B> inline void maximize(A& x, B y) {x = max(x, y);};
-template<class A,class B> inline void minimize(A& x, B y) {x = min(x, y);};
-
-const int N = 2e5 + 68;
-const int mod = 1e9 + 7;
-const int inf = 2e9 + 1e8 + 6688;
-const ll oo = 3e18 + 1e17 + 666888;
-const bool TESTCASE = false;
-
-//  ------------------- d a t m a . _ c o d e r -------------------  //
+const int maxn = 7 + 2;
+const int dx[] = {0 , 0 , 1 , -1};
+const int dy[] = {1 , -1 , 0 , 0};
 
 string s;
-bool visited[10][10];
+bool vis[maxn][maxn];
 
-bool check(int i , int j) {
-        if (visited[i][j] == true) return true;
+bool check(int x , int y) {
+        if (vis[x][y] == true) return true;
 
         int cnt = 0;
 
-        if (i - 1 > 0 && !visited[i - 1][j]) ++cnt;
-        if (i + 1 < 8 && !visited[i + 1][j]) ++cnt;
-        if (j - 1 > 0 && !visited[i][j - 1]) ++cnt;
-        if (j + 1 < 8 && !visited[i][j + 1]) ++cnt;
+        for (int k = 0 ; k < 4 ; k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
 
-        if (i == 7 && j == 1 && cnt > 0)
-                return true;
+                if (nx > 0 && ny > 0 && nx <= 7 && ny <= 7 && !vis[nx][ny]) ++cnt;
+        }
 
-        if (cnt < 2) 
-                return false; 
+        if (x == 7 && y == 1 && cnt > 0) return true;
+
+        if (cnt < 2) return false;
 
         return true;
 }
 
-bool Check(int i , int j) {
+bool CHECK(int x , int y) {
         int col = 0 , row = 0;
 
-        if (i - 1 > 0 && !visited[i - 1][j]) ++row;
-        if (i + 1 < 8 && !visited[i + 1][j]) ++row;
+        if (x - 1 > 0 && !vis[x - 1][y]) ++row;
+        if (x + 1 <= 7 && !vis[x + 1][y]) ++row;
 
-        if (j - 1 > 0 && !visited[i][j - 1]) ++col;
-        if (j + 1 < 8 && !visited[i][j + 1]) ++col;
+        if (y - 1 > 0 && !vis[x][y - 1]) ++col;
+        if (y + 1 <= 7 && !vis[x][y + 1]) ++col;
 
         if (row == 2 && col == 0) return false;
         if (row == 0 && col == 2) return false;
@@ -58,90 +43,71 @@ bool Check(int i , int j) {
         return true;
 }
 
-bool CrossCheck(int i , int j) {
-        if (i - 1 > 0 && j - 1 > 0) 
-                if (!check(i - 1 , j - 1)) return false;
+bool CrossCheck(int x , int y) {
+        if (x - 1 > 0 && y - 1 > 0) 
+                if (!check(x - 1 , y - 1)) return false;
+        
+        if (x - 1 > 0 && y + 1 <= 7) 
+                if (!check(x - 1 , y + 1)) return false;
 
-        if (i - 1 > 0 && j + 1 < 8) 
-                if (!check(i - 1 , j + 1)) return false;
-
-        if (i + 1 < 8 && j - 1 > 0) 
-                if (!check(i + 1 , j - 1)) return false;
-    
-        if (i + 1 < 8 && j + 1 < 8) 
-                if (!check(i + 1 , j + 1)) return false;
-
+        if (x + 1 <= 7 && y - 1 > 0) 
+                if (!check(x + 1 , y - 1)) return false;
+        
+        if (x + 1 <= 7 && y + 1 <= 7) 
+                if (!check(x + 1 , y + 1)) return false;
+        
         return true;
 }
 
 int ans = 0;
 
-void rec(int idx , int i , int j) {
-        if (visited[i][j] == true) return;
+void rec(int idx , int x , int y) {
+        if (vis[x][y] == true) return;
 
-        if (i == 0 || i == 8 || j == 0 || j == 8) return;
+        if (x == 0 || x == 8 || y == 0 || y == 8) return;
 
-        if (idx < 7 * 7 && i == 7 && j == 1) return;
+        if (idx < 7 * 7 && x == 7 && y == 1) return;
 
-        visited[i][j] = true;
+        vis[x][y] = true;
 
-        if (!CrossCheck(i , j)) {
-                visited[i][j] = false; 
-
+        if (!CrossCheck(x , y)) {
+                vis[x][y] = false;
                 return;
         }
 
-        if (!Check(i , j)) {
-                visited[i][j] = false;
-
+        if (!CHECK(x , y)) {
+                vis[x][y] = false;
                 return;
         }
 
-        if (idx == 7 * 7) ++ans;
+        if (idx == 7 * 7) 
+                ++ans;
         else 
-                if (s[idx] == '?')
-                        rec(idx + 1 , i + 1 , j) ,
-                        rec(idx + 1 , i , j + 1) ,
-                        rec(idx + 1 , i - 1 , j) ,
-                        rec(idx + 1 , i , j - 1);
-        
+                if (s[idx] == '?') 
+                        for (int k = 0 ; k < 4 ; k++) 
+                                rec(idx + 1 , x + dx[k] , y + dy[k]);
                 else 
-                        if (s[idx] == 'U') rec(idx + 1 , i - 1 , j);
-                        else if (s[idx] == 'D') rec(idx + 1 , i + 1 , j);
-                        else if (s[idx] == 'L') rec(idx + 1 , i , j - 1);
-                        else if (s[idx] == 'R') rec(idx + 1 , i , j + 1);
-
-        visited[i][j] = false;
+                        if (s[idx] == 'U') rec(idx + 1 , x - 1 , y);
+                        else if (s[idx] == 'D') rec(idx + 1 , x + 1 , y);
+                        else if (s[idx] == 'L') rec(idx + 1 , x , y - 1);
+                        else if (s[idx] == 'R') rec(idx + 1 , x , y + 1);
+                
+        vis[x][y] = false;
 }
 
-void solve() {
-        cin >> s; s = " " + s;
+int32_t main() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0); cout.tie(0);
 
-        reset(visited , false);
+        cin >> s;
+
+        s = " " + s;
+
+        memset(vis , false , sizeof vis);
 
         rec(1 , 1 , 1);
 
         cout << ans;
-}
-
-datmacoder {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL); cout.tie(NULL);
-
-        openfile("txt");
-
-        int testcase;
-        if (!TESTCASE) testcase = 1;
-        else cin >> testcase;
-
-        while (testcase--) {
-                solve();
-        }
 
         return 0;
 }
-
-/* 
-        d a t m a . _ c o d e r
-        H O A N G   T H E   T H A N H   D A T
-*/
