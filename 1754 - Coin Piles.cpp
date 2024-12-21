511dd92367aeb1b3f3df96fa8e25a9b844388f9c
d a t m a . _ c o d e r <121338679+HoangTheThanhDat@users.noreply.github.com>
2024-12-21 21:45:28 +0700

    Update 1754 - Coin Piles.cpp

--- 1754 - Coin Piles.cpp
+++ 1754 - Coin Piles.cpp
@@ -2,54 +2,22 @@
 
 using namespace std;
 
-#define ll long long
-#define fi first 
-#define se second 
-#define el '\n'
-#define reset(__a , __v) memset(__a,__v,sizeof(__a))
-#define datmacoder int32_t main()
-#define openfile(__dat) if (fopen(__dat".inp","r")){freopen(__dat".inp","r",stdin);freopen(__dat".out","w",stdout);}
+int t , a , b;
 
-template<class A,class B> inline void maximize(A& x, B y) {x = max(x, y);};
-template<class A,class B> inline void minimize(A& x, B y) {x = min(x, y);};
-
-const int N = 2 * 1e5 + 68;
-const int mod = 1e9 + 7;
-const int inf = 2 * 1e9 + 1e8 + 6688;
-const ll oo = 3 * 1e18 + 1e17 + 666888;
-const bool TESTCASE = true;
-
-//  ------------------- d a t m a . _ c o d e r -------------------  //
-
-int a , b;
-
-void solve() {
-        cin >> a >> b;
-
-        if ((2 * a - b) % 3 || (2 * a - b) < 0 || (2 * b - a) % 3 || (2 * b - a) < 0) 
-                cout << "NO" << el;
-        else 
-                cout << "YES" << el;
-}
-
-datmacoder {
+int32_t main() {
         ios_base::sync_with_stdio(false);
-        cin.tie(NULL); cout.tie(NULL);
+        cin.tie(0); cout.tie(0);
 
-        openfile("txt");
+        cin >> t;
 
-        int testcase;
-        if (!TESTCASE) testcase = 1;
-        else cin >> testcase;
+        while (t--) {
+                cin >> a >> b;
 
-        while (testcase--) {
-                solve();
+                if ((2 * a - b) % 3 != 0 || (2 * a - b) < 0 || (2 * b - a) % 3 != 0 || (2 * b - a) < 0)      
+                        cout << "NO" << '\n';
+                else 
+                        cout << "YES" << '\n';
         }
 
         return 0;
 }
-
-/* 
-        d a t m a . _ c o d e r
-        H O A N G   T H E   T H A N H   D A T
-*/
