b691b836aa0d2f8a95c87e0e99754f380c523d6f
d a t m a . _ c o d e r <121338679+HoangTheThanhDat@users.noreply.github.com>
2024-12-21 23:26:24 +0700

    Update 1091 - Concert Tickets.cpp

--- 1091 - Concert Tickets.cpp
+++ 1091 - Concert Tickets.cpp
@@ -2,66 +2,29 @@
 
 using namespace std;
 
-#define ll long long
-#define fi first 
-#define se second 
-#define el '\n'
-#define reset(__a , __v) memset(__a,__v,sizeof(__a))
-#define datmacoder int32_t main()
-#define openfile(__dat) if (fopen(__dat".inp","r")){freopen(__dat".inp","r",stdin);freopen(__dat".out","w",stdout);}
-
-template<class A,class B> inline void maximize(A& x, B y) {x = max(x, y);};
-template<class A,class B> inline void minimize(A& x, B y) {x = min(x, y);};
-
-const int N = 2e5 + 68;
-const int mod = 1e9 + 7;
-const int inf = 2e9 + 1e8 + 6688;
-const ll oo = 3e18 + 1e17 + 666888;
-const bool TESTCASE = false;
-
-//  ------------------- d a t m a . _ c o d e r -------------------  //
-
 int n , m , h , t;
 set<pair<int , int> , greater<pair<int , int>>> s;
 
-void solve() {
+int32_t main() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0); cout.tie(0);
+
         cin >> n >> m;
 
-        for (int i = 1 ; i <= n ; i++) cin >> h , s.insert({h , i});
+        for (int i = 1 ; i <= n ; i++) cin >> h , s.insert(make_pair(h , i));
 
         for (int i = 1 ; i <= m ; i++) {
                 cin >> t;
 
-                set<pair<int , int> , greater<pair<int , int>>>::iterator x = s.lower_bound({t , n + 1});
+                set<pair<int , int> , greater<pair<int , int>>>::iterator x = s.lower_bound(make_pair(t , n + 1));
 
                 if (x == s.end()) 
-                        cout << -1 << el;
+                        cout << -1 << '\n';
                 else {
-                        cout << x -> fi << el;
-
+                        cout << x->first << '\n';
                         s.erase(x);
                 }
-        }
-}
-
-datmacoder {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL); cout.tie(NULL);
-
-        openfile("txt");
-
-        int testcase;
-        if (!TESTCASE) testcase = 1;
-        else cin >> testcase;
-
-        while (testcase--) {
-                solve();
         }
 
         return 0;
 }
-
-/* 
-        d a t m a . _ c o d e r
-        H O A N G   T H E   T H A N H   D A T
-*/
