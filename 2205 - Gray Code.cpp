a35b89aabb91861939453bcbf67486738e645eb6
d a t m a . _ c o d e r <121338679+HoangTheThanhDat@users.noreply.github.com>
2024-12-21 22:12:05 +0700

    Update 2205 - Gray Code.cpp

--- 2205 - Gray Code.cpp
+++ 2205 - Gray Code.cpp
@@ -2,29 +2,13 @@
 
 using namespace std;
 
-#define ll long long
-#define fi first 
-#define se second 
-#define el '\n'
-#define reset(__a , __v) memset(__a,__v,sizeof(__a))
-#define datmacoder int32_t main()
-#define openfile(__dat) if (fopen(__dat".inp","r")){freopen(__dat".inp","r",stdin);freopen(__dat".out","w",stdout);}
-
-template<class A,class B> inline void maximize(A& x, B y) {x = max(x, y);};
-template<class A,class B> inline void minimize(A& x, B y) {x = min(x, y);};
-
-const int N = 2 * 1e5 + 68;
-const int mod = 1e9 + 7;
-const int inf = 2 * 1e9 + 1e8 + 6688;
-const ll oo = 3 * 1e18 + 1e17 + 666888;
-const bool TESTCASE = false;
-
-//  ------------------- d a t m a . _ c o d e r -------------------  //
-
 int n;
 vector<string> vec;
 
-void solve() {
+int32_t main() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0); cout.tie(0);
+
         cin >> n;
 
         vec.push_back("");
@@ -38,32 +22,12 @@ void solve() {
 
                 for (int j = 0 ; j < sz ; j++) 
                         if (j < sz / 2) 
-                                vec[j] = "0" + vec[j];
+                                vec[j] = '0' + vec[j];
                         else 
-                                vec[j] = "1" + vec[j];
+                                vec[j] = '1' + vec[j];
         }
 
-        for (int i = 0 ; i < vec.size() ; i++) cout << vec[i] << el;
-}
-
-datmacoder {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL); cout.tie(NULL);
-
-        openfile("txt");
-
-        int testcase;
-        if (!TESTCASE) testcase = 1;
-        else cin >> testcase;
-
-        while (testcase--) {
-                solve();
-        }
+        for (int i = 0 ; i < vec.size() ; i++) cout << vec[i] << '\n';
 
         return 0;
 }
-
-/* 
-        d a t m a . _ c o d e r
-        H O A N G   T H E   T H A N H   D A T
-*/
