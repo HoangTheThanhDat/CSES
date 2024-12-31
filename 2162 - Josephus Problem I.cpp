a7c429009b1fecd1e6c7238448bc31923ecc4679
d a t m a . _ c o d e r <121338679+HoangTheThanhDat@users.noreply.github.com>
2024-12-31 22:39:44 +0700

    Update 2162 - Josephus Problem I.cpp

--- 2162 - Josephus Problem I.cpp
+++ 2162 - Josephus Problem I.cpp
@@ -2,85 +2,45 @@
 
 using namespace std;
 
-#define ll long long
-#define fi first 
-#define se second 
-#define el '\n'
-#define reset(__a , __v) memset(__a,__v,sizeof(__a))
-#define datmacoder int32_t main()
-#define openfile(__dat) if (fopen(__dat".inp","r")){freopen(__dat".inp","r",stdin);freopen(__dat".out","w",stdout);}
+const int maxn = 2e5 + 68;
 
-template<class A,class B> inline void maximize(A& x, B y) {x = max(x, y);};
-template<class A,class B> inline void minimize(A& x, B y) {x = min(x, y);};
+int n , idx;
+bool vis[maxn];
 
-const int N = 2e5 + 68;
-const int mod = 1e9 + 7;
-const int inf = 2e9 + 1e8 + 6688;
-const ll oo = 3e18 + 1e17 + 666888;
-const bool TESTCASE = false;
+int32_t main() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0); cout.tie(0);
 
-//  ------------------- d a t m a . _ c o d e r -------------------  //
-
-int n , i;
-bool visited[N];
-
-void solve() {
         cin >> n;
 
         if (n == 1) {
                 cout << 1;
-                return;
+                return 0;
         }
 
-        i = 2; 
+        idx = 2;
 
         int c = 0;
 
-        reset(visited , false);
+        memset(vis , false , sizeof vis);
 
         while (c < n) {
                 ++c;
 
-                cout << i << " ";
+                cout << idx << ' ';
 
-                if (c == n) return;
+                if (c == n) return 0;
 
-                visited[i] = true;
+                vis[idx] = true;
 
-                ++i; if (i == n + 1) i = 1;
+                idx = (idx == n ? 1 : idx + 1);
 
-                while (visited[i]) {
-                        ++i;
-                        if (i == n + 1) i = 1;
-                }
+                while (vis[idx]) idx = (idx == n ? 1 : idx + 1);
 
-                ++i; if (i == n + 1) i = 1;
-
-                while (visited[i]) {
-                        ++i;
-                        if (i == n + 1) i = 1;
-                }
-        }
-}
-
-datmacoder {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL); cout.tie(NULL);
-
-        openfile("txt");
-
-        int testcase;
-        if (!TESTCASE) testcase = 1;
-        else cin >> testcase;
-
-        while (testcase--) {
-                solve();
+                idx = (idx == n ? 1 : idx + 1);
+                
+                while (vis[idx]) idx = (idx == n ? 1 : idx + 1);
         }
 
         return 0;
 }
-
-/* 
-        d a t m a . _ c o d e r
-        H O A N G   T H E   T H A N H   D A T
-*/
